5d3fda3bd6bacd85eed154ea9f4c7531171e18e8
csjohn1 <csjohn@shaw.ca>
2020-09-24 03:38:46 +0000

    adding fetcher

---
+++ fetcher.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const request = require('request');
+
+const fetch = process.argv.slice(2);
+
+request('http://www.example.edu', (error, response, body) => {
+  fs.writeFile(fetch[1], body, function(err) {
+    if (err) throw err;
+    console.log('Great Success!');
+    });
+});
+
+
+
